2213f5253980bc8e98ebba20b72c6e2d27e95697
FinSpark Dev <dev@finspark.com>
2026-04-05 01:58:21 +0530

    Remove authentication from frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import NewIntegration from './pages/NewIntegration'
 import APIRegistry from './pages/APIRegistry'
@@ -9,12 +10,12 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/dashboard" replace />} />
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="new-integration" element={<NewIntegration />} />
         <Route path="api-registry" element={<APIRegistry />} />
         <Route path="configurations" element={<Configurations />} />
       </Route>
     </Routes>
-  )
 }
